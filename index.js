77c1e430d2d2b8a200adb99901a689172c89651e
YashKhandelwal-tc <yash.khandelwal@techconfer.in>
2023-11-11 16:36:55 +0530

    assesment Listed

---
+++ index.js
@@ -0,0 +1,205 @@
+const express = require('express');
+const cookieParser = require('cookie-parser');
+const app = express();
+const port = 8000;
+const expressLayouts = require('express-ejs-layouts');
+const db = require('./config/mongoose');
+// used for session cookie
+const session = require('express-session');
+const passport = require('passport');
+const passportLocal = require('./config/passport-local-strategy');
+const MongoStore = require('connect-mongo')(session);
+
+const passportGoogle = require('./config/passport-google-oauth2');
+
+app.use(express.urlencoded());
+
+app.use(cookieParser());
+
+app.use(express.static('./assets'));
+
+app.use(expressLayouts);
+// extract style and scripts from sub pages into the layout
+app.set('layout extractStyles', true);
+app.set('layout extractScripts', true);
+
+
+
+
+// set up the view engine
+app.set('view engine', 'ejs');
+app.set('views', './views');
+
+// mongo store is used to store the session cookie in the db
+app.use(session({
+    name: 'codeial',
+    secret: 'blahsomething',
+    saveUninitialized: false,
+    resave: false,
+    cookie: {
+        maxAge: (1000 * 60 * 100)
+    },
+    store: new MongoStore(
+        {
+            mongooseConnection: db,
+            autoRemove: 'disabled'
+        
+        },
+        function(err){
+            console.log(err ||  'connect-mongodb setup ok');
+        }
+    )
+}));
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+app.use(passport.setAuthenticatedUser);
+
+// use express router
+app.use('/', require('./routes'));
+const { authenticate } = require("@google-cloud/local-auth");
+const fs = require("fs").promises;
+const { google } = require("googleapis");
+
+const SCOPES = [
+    "https://www.googleapis.com/auth/gmail.readonly",
+    "https://www.googleapis.com/auth/gmail.send",
+    "https://www.googleapis.com/auth/gmail.labels",
+    "https://mail.google.com/",
+  ];
+
+
+const labelName = "Vacation Auto-Reply";
+
+
+app.get("/", async (req, res) => {
+
+  const auth = await authenticate({
+    keyfilePath: path.join(__dirname, "Credential.json"),
+    scopes: SCOPES,
+  });
+
+  const gmail = google.gmail({ version: "v1", auth });
+
+  const response = await gmail.users.labels.list({
+    userId: "me",
+  });
+
+
+
+  async function getUnrepliesMessages(auth) {
+    const gmail = google.gmail({ version: "v1", auth });
+    const response = await gmail.users.messages.list({
+      userId: "me",
+      labelIds: ["INBOX"],
+      q: "is:unread",
+    });
+    
+    return response.data.messages || [];
+  }
+
+
+  async function createLabel(auth) {
+    const gmail = google.gmail({ version: "v1", auth });
+    try {
+      const response = await gmail.users.labels.create({
+        userId: "me",
+        requestBody: {
+          name: labelName,
+          labelListVisibility: "labelShow",
+          messageListVisibility: "show",
+        },
+      });
+      return response.data.id;
+    } catch (error) {
+      if (error.code === 409) {
+        const response = await gmail.users.labels.list({
+          userId: "me",
+        });
+        const label = response.data.labels.find(
+          (label) => label.name === labelName
+        );
+        return label.id;
+      } else {
+        throw error;
+      }
+    }
+  }
+
+  async function main() {
+
+    const labelId = await createLabel(auth);
+
+    setInterval(async () => {
+ 
+      const messages = await getUnrepliesMessages(auth);
+
+      if (messages && messages.length > 0) {
+        for (const message of messages) {
+          const messageData = await gmail.users.messages.get({
+            auth,
+            userId: "me",
+            id: message.id,
+          });
+
+          const email = messageData.data;
+          const hasReplied = email.payload.headers.some(
+            (header) => header.name === "In-Reply-To"
+          );
+
+          if (!hasReplied) {
+            const replyMessage = {
+              userId: "me",
+              resource: {
+                raw: Buffer.from(
+                  `To: ${
+                    email.payload.headers.find(
+                      (header) => header.name === "From"
+                    ).value
+                  }\r\n` +
+                    `Subject: Re: ${
+                      email.payload.headers.find(
+                        (header) => header.name === "Subject"
+                      ).value
+                    }\r\n` +
+                    `Content-Type: text/plain; charset="UTF-8"\r\n` +
+                    `Content-Transfer-Encoding: 7bit\r\n\r\n` +
+                    `Thank you for your email. I'm currently on vacation and will reply to you when I return.\r\n`
+                ).toString("base64"),
+              },
+            };
+
+            await gmail.users.messages.send(replyMessage);
+            await gmail.users.messages.modify({
+              auth,
+              userId: "me",
+              id: message.id,
+              resource: {
+                addLabelIds: [labelId],
+                removeLabelIds: ["INBOX"],
+              },
+            });
+          }
+        }
+      }
+    }, Math.floor(Math.random() * (120 - 45 + 1) + 45) * 1000);
+  }
+
+
+  
+  main();
+  // const labels = response.data.labels;
+  res.json({ "this is Auth": auth });
+});
+
+// app.listen(port, () => {
+//   console.log(`server is running ${port}`);
+// });
+app.listen(port, function(err){
+    if (err){
+        console.log(`Error in running the server: ${err}`);
+    }
+
+    console.log(`Server is running on port: ${port}`);
+});
